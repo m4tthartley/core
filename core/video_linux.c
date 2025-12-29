da29847fac704efcafc44e2ac308af78bc7e2312
Matt Hartley <m4tthartley@gmail.com>
2025-12-29 03:02:05 +0000

    More linux window stuff

--- core/video_linux.c
+++ core/video_linux.c
@@ -5,12 +5,13 @@
 
 #include "video.h"
 
+#include <unistd.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <GL/gl.h>
 // NOTE: I have to do this to stop it erroring for some reason
-typedef signed long int GLintptr;
-typedef signed long int GLsizeiptr;
+// typedef signed long int GLintptr;
+// typedef signed long int GLsizeiptr;
 #include <GL/glx.h>
 
 
@@ -18,8 +19,17 @@ typedef signed long int GLsizeiptr;
 #define FALSE 0
 
 
+void __vid_print(char* str) {
+	int len = 0;
+	while (str[len]) ++len;
+	write(STDOUT_FILENO, str, len);
+}
+
 CORE window_t vid_init_window(char* title, int width, int height, int flags)
 {
+	// TODO: Error handling
+	// TODO: Maybe switch to EGL
+
 	window_t vid = {
 		.width = width,
 		.height = height,
@@ -28,12 +38,32 @@ CORE window_t vid_init_window(char* title, int width, int height, int flags)
 
 	Display* display = XOpenDisplay(NULL);
 
-	Window parent = XDefaultRootWindow(display);
-	Window window = XCreateSimpleWindow(display, parent, 100, 100, width, height, 0, 0, 0);
+	Window root = XDefaultRootWindow(display);
+	int screen = XDefaultScreen(display);
+
+	XVisualInfo visualInfo;
+	if (!XMatchVisualInfo(display, screen, 24, TrueColor, &visualInfo)) {
+		__vid_print("XMatchVisualInfo failed \n");
+		exit(1);
+	}
+
+	XSetWindowAttributes attrs = {
+		.background_pixel = 0,
+		.colormap = XCreateColormap(display, root, visualInfo.visual, AllocNone),
+	};
+	unsigned long attrMask = CWBackPixel | CWColormap;
+
+	Window window = XCreateWindow(
+		display, root,
+		0, 0,
+		width, height, 0,
+		visualInfo.depth, InputOutput, visualInfo.visual, attrMask, &attrs
+	);
+	// Window window = XCreateSimpleWindow(display, parent, 100, 100, width, height, 0, 0, 0);
 
 	XStoreName(display, window, title);
 
-	XSelectInput(display, window, KeyPressMask | KeyReleaseMask | ClientMessage | StructureNotifyMask);
+	XSelectInput(display, window, KeyPressMask | KeyReleaseMask | ClientMessage | StructureNotifyMask | ResizeRedirectMask);
 
 	XMapWindow(display, window);
 	XFlush(display);
@@ -56,6 +86,10 @@ CORE void vid_poll_events(window_t* vid)
 		switch (event.type) {
 			case DestroyNotify:
 				vid->quit = TRUE;
+				break;
+			case ResizeRequest:
+				// vid->width = event.xresizerequest.width;
+				// vid->height = event.xresizerequest.height;
 				break;
 
 			case KeyPress:
