cb620b08ff1a4e3c311ed244e19ea9e6e8179b53
Matt Hartley <m4tthartley@gmail.com>
2026-01-13 15:15:47 +0000

    Linux time_get_ms

--- core/time_linux.c
+++ core/time_linux.c
@@ -18,3 +18,15 @@ double time_get_seconds() {
 
 	return result;
 }
+
+double time_get_ms() {
+	struct timespec ts;
+	clock_gettime(CLOCK_MONOTONIC, &ts);
+
+	// uint64_t ns = (ts.tv_sec * 1000000000) + ts.tv_nsec;
+	// double result = (double)ns / 1000000000;
+
+	double result = (double)ts.tv_sec + ((double)ts.tv_nsec / 1000000.0);
+
+	return result;
+}
